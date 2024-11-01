17f3cbc193822073fdb2a9b38dd3e7b6d937cc18
jemorais <jemorais@c2r10p5.42sp.org.br>
2024-11-01 19:28:58 -0300

    start ft_printf

---
+++ 42cursus/ft_printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jemorais <jemorais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 12:08:27 by jemorais          #+#    #+#             */
+/*   Updated: 2024/11/01 11:15:33 by jemorais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
