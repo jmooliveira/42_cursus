8f783217794e781ce7395d5945f71f1f62686436
jemorais <jemorais@c2r9p1.42sp.org.br>
2024-11-14 17:19:33 -0300

    try GNL

--- 42cursus/get_next_line/get_next_line.h
+++ 42cursus/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jemorais <jemorais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:16:09 by jemorais          #+#    #+#             */
-/*   Updated: 2024/11/13 20:22:36 by jemorais         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:48:59 by jemorais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 # define GET_NEXT_LINE_H
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 1
 #endif
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
 char	*get_next_line(int fd);
